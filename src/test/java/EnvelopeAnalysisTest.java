f037c3691c0186e2a948f44f99d34c76bd79c39f
zaets39 <katezytchuk24@gmail.com>
2019-10-17 15:40:07 +0300

    Tests for negative dimention values created

--- src/test/java/EnvelopeAnalysisTest.java
+++ src/test/java/EnvelopeAnalysisTest.java
@@ -11,27 +11,60 @@ import static org.junit.Assert.*;
 
 public class EnvelopeAnalysisTest {
     @Test(expected = IllegalArgumentException.class)
-    public void getAnalysisAllZeroDataTest()
+    public void getAnalysisAllZeroNumsTest()
     {
         double [] allZeroCheck = new double[] {0, 0, 0, 0};
         EnvelopeAnalysis.getAnalysis(allZeroCheck);
     }
+
     @Test(expected = IllegalArgumentException.class)
-    public void getAnalysisTwoZeroDataTest()
+    public void getAnalysisTwoZeroNumsTest()
     {
         double [] twoZeroCheck = new double[] {0, 6, 0, 5};
         EnvelopeAnalysis.getAnalysis(twoZeroCheck);
     }
+
     @Test(expected = IllegalArgumentException.class)
-    public void getAnalysisThreeZeroDataTest()
+    public void getAnalysisThreeZeroNumsTest()
     {
         double [] threeZeroCheck = new double[] {0, 0, 0, 5};
         EnvelopeAnalysis.getAnalysis(threeZeroCheck);
     }
+
     @Test(expected = IllegalArgumentException.class)
-    public void getAnalysisOneZeroDataTest()
+    public void getAnalysisOneZeroNumTest()
     {
         double [] oneZeroCheck = new double[] {0, 3, 4, 5};
         EnvelopeAnalysis.getAnalysis(oneZeroCheck);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getAnalysisOneNegativeNumTest()
+    {
+        double [] oneNegativeCheck = new double[] {-5, 8, 42, 5};
+        EnvelopeAnalysis.getAnalysis(oneNegativeCheck);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getAnalysisTwoNegativeNumsTest()
+    {
+        double [] twoNegativeCheck = new double[] {-8, -34, 6, 25};
+        EnvelopeAnalysis.getAnalysis(twoNegativeCheck);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getAnalysisThreeNegativeNumsTest()
+    {
+        double [] threeNegativeCheck = new double[] {-8, -34, 6, -65};
+        EnvelopeAnalysis.getAnalysis(threeNegativeCheck);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getAnalysisAllNegativeNumsTest()
+    {
+        double [] allNegativeCheck = new double[] {-8, -34, -6, -12};
+        EnvelopeAnalysis.getAnalysis(allNegativeCheck);
+    }
+
+    
 }
