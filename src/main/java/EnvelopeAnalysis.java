2fe5d489ffb31b31ff6dd7e3f842e8098aedb690
zaets39 <katezytchuk24@gmail.com>
2019-10-18 13:38:07 +0300

    Other conditions added

--- src/main/java/EnvelopeAnalysis.java
+++ src/main/java/EnvelopeAnalysis.java
@@ -8,13 +8,23 @@
 public class EnvelopeAnalysis {
 
     public  static String getAnalysis(double firstWidth, double firstLength,
-                                      double secondWidth, double secondLength) {
+                                      double secondWidth, double secondLength){
         String result = "";
         if((firstWidth == secondWidth) || (firstLength == secondLength)
             || (firstLength == secondWidth) || (firstWidth == secondLength)){
-            result = "You can`t put one envelope to another";
+            result = "Your envelopes are equal so, you can`t put one envelope to another";
         }
-        
+        else  if (((firstWidth > secondWidth) && (firstLength > secondLength))
+                ||((firstWidth > secondLength) && (firstLength > secondWidth))){
+            result = "You can put your second envelope to first";
+        }
+        else if (((secondWidth > firstWidth) && (secondLength > firstLength)) ||
+                ((secondWidth > firstLength)&&(secondLength > firstWidth))){
+            result = "You can put your first envelope to second";
+        }
+        else{
+            result = "You can`t put your one envelope to another";
+        }
         return result;
     }
 
