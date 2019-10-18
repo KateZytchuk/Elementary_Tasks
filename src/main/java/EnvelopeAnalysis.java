82ae5ce626ea49d7321b9f05acf3b1b869d1e0ab
zaets39 <katezytchuk24@gmail.com>
2019-10-18 15:03:29 +0300

    All tests are passed

--- src/main/java/EnvelopeAnalysis.java
+++ src/main/java/EnvelopeAnalysis.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by IntelliJ IDEA.
  * USER: zaets39
@@ -8,8 +10,16 @@
 public class EnvelopeAnalysis {
 
     public  static String getAnalysis(double firstWidth, double firstLength,
-                                      double secondWidth, double secondLength){
+                                      double secondWidth, double secondLength)
+                                    throws IllegalArgumentException{
+
         String result = "";
+
+        if(firstWidth <= 0 || firstLength <= 0 || secondWidth <=0
+                || secondLength <=0){
+            throw  new IllegalArgumentException();
+        }
+
         if((firstWidth == secondWidth) && (firstLength == secondLength)
             || (firstLength == secondWidth) && (firstWidth == secondLength)){
             result = "Your envelopes are equal so, you can`t put one envelope to another";
@@ -27,5 +37,25 @@ public class EnvelopeAnalysis {
         }
         return result;
     }
-
+    public static void inputDementions()
+    {
+        double firstWidth, firstLength, secondWidth, secondLength;
+        String answer;
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Enter the width of first envelope: ");
+            firstWidth = Double.parseDouble(in.nextLine());
+            System.out.println("Enter the length of first envelope: ");
+            firstLength = Double.parseDouble(in.nextLine());
+            System.out.println("Enter the width of second envelope: ");
+            secondWidth = Double.parseDouble(in.nextLine());
+            System.out.println("Enter the length of second envelope: ");
+            secondLength = Double.parseDouble(in.nextLine());
+            answer = getAnalysis(firstWidth, firstLength, secondWidth, secondLength);
+            System.out.println(answer);
+        } catch (Exception ex)
+        {
+            //TODO;
+        }
+    }
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -7,6 +7,6 @@
 
 public class Program {
     public static void main(String[] args) {
-
+            EnvelopeAnalysis.inputDementions();
     }
 }
