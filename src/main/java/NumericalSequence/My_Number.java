e851b86c88ca20d7d76b6578fa096b347bde2fa9
zaets39 <katezytchuk24@gmail.com>
2019-10-20 16:46:26 +0300

    Menu added

---
+++ src/main/java/NumericalSequence/My_Number.java
@@ -0,0 +1,11 @@
+package NumericalSequence;
+
+/**
+ * Created by IntelliJ IDEA.
+ * USER: zaets39
+ * DATE: 20.10.2019
+ * TIME: 15:55
+ */
+
+public class My_Number {
+}
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -14,4 +14,17 @@ public class Program {
         ChessOperations.getBoard();
     }
 
+    static void showMenu(){
+        System.out.println("Choose the program you want to execute:");
+        System.out.println("1)Chess Board");
+        System.out.println("2)Envelopes Analysis");
+        System.out.println("3)Sorting of triangles");
+        System.out.println("4)File parser");
+        System.out.println("5)Number in words");
+        System.out.println("6)Lucky tickets");
+        System.out.println("7)Numeric sequence");
+        System.out.println("8)Fibonacci series for range");
+        System.out.println("9)Exponentiation");
+        System.out.println("10)Storage");
+    }
 }
