894871e9d23e4911620970d0bdc271bb8396276b
zaets39 <katezytchuk24@gmail.com>
2019-10-21 16:51:55 +0300

    Trying to do task with Fibonacci range

---
+++ src/main/java/fibonacci/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package fibonacci;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * USER: zaets39
+ * DATE: 21.10.2019
+ * TIME: 16:06
+ */
+
+public class FibonacciSeries {
+
+    private List<Integer> inRangeFibonacci = new ArrayList<Integer>();
+
+     List findInRange(int start, int end)
+    {
+        
+    }
+}
--- src/test/java/numerical_sequence/My_NumberTest.java
+++ src/test/java/numerical_sequence/My_NumberTest.java
@@ -14,28 +14,28 @@ import static org.junit.Assert.*;
 public class My_NumberTest {
     @Test
     public void twoInputTest() {
-        int [] actual = My_Number.findNumbers(2);
+        int [] actual = MyNumber.findNumbers(2);
         int [] expected = new int[]{1};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void twentyFiveInputTest() {
-        int [] actual = My_Number.findNumbers(25);
+        int [] actual = MyNumber.findNumbers(25);
         int [] expected = new int[]{1, 2, 3, 4, 5};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void thirtyThreeInputTest() {
-        int [] actual = My_Number.findNumbers(33);
+        int [] actual = MyNumber.findNumbers(33);
         int [] expected = new int[]{1, 2, 3, 4, 5};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void fiftyInputTest() {
-        int [] actual = My_Number.findNumbers(50);
+        int [] actual = MyNumber.findNumbers(50);
         int [] expected = new int[]{1, 2, 3, 4, 5, 6, 7};
         assertArrayEquals(expected, actual);
     }
